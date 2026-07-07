88fe936bd94b166a7e5ae7b203ad5ac5422870de
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-07-07 15:11:37 -0300

    feat: Remove o caminho da URL administrativa da configuração de URL do projeto.

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.permissions import AllowAny
@@ -23,7 +22,6 @@ from rest_framework.permissions import AllowAny
 BASE_URL_API = "api/v1/"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path(
         "api/v1/schema/",
         SpectacularAPIView.as_view(
