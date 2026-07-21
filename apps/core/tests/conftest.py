e8616fc1ea523394d17bd454e4deece0ddfeb0d1
brunosp1024 <brunosalvador145@gmail.com>
2026-07-21 13:25:23 -0300

    test: ajusta model de teste no conftest do app core

--- apps/core/tests/conftest.py
+++ apps/core/tests/conftest.py
@@ -1,12 +1,8 @@
-import uuid
-
 import pytest
-from django.conf import settings
 from django.db import connection, models
-from django.utils import timezone
 from rest_framework.test import APIRequestFactory
 
-from apps.core.models.mixins import CustomManager
+from apps.core.models.mixins import BaseModel
 
 
 @pytest.fixture
@@ -15,47 +11,13 @@ def api_factory():
     return APIRequestFactory()
 
 
-class ModelBase(models.Model):
+class ModelBase(BaseModel):
     """Modelo de teste para BaseModel."""
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    criado_em = models.DateTimeField(auto_now_add=True)
-    atualizado_em = models.DateTimeField(auto_now=True)
-    criado_por = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="core_modelbase_criado",
-    )
-    atualizado_por = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="core_modelbase_atualizado",
-    )
-    deletado_em = models.DateTimeField(
-        "Deletado em", default=None, null=True, blank=True
-    )
     nome = models.CharField(max_length=100)
-
-    objects = CustomManager()
-    dm_objects = models.Manager()
 
     class Meta:
         app_label = "core"
-
-    def delete(self, user=None):
-        self.deletado_em = timezone.now()
-        self.save(update_fields=["deletado_em"])
-
-    def hard_delete(self, using=None, keep_parents=False):
-        return super().delete(using=using, keep_parents=keep_parents)
-
-    def restore(self):
-        self.deletado_em = None
-        self.save(update_fields=["deletado_em"])
 
 
 @pytest.fixture(scope="session", autouse=True)
