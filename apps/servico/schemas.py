ffbf7cbf79ab1ac1670321a92e004d656d423053
Matheus Bonaretti <bona.matheuss2@gmail.com>
2026-07-24 13:26:39 -0300

    feat(151052): Ajustado o schema

--- apps/servico/schemas.py
+++ apps/servico/schemas.py
@@ -7,28 +7,36 @@ from drf_spectacular.utils import (
     extend_schema_view,
 )
 
+from apps.servico.serializers import ServicoCriarSerializer
+
 SERVICO_SCHEMA = extend_schema_view(
     create=extend_schema(
         tags=["Serviço"],
         summary="Cria um novo serviço",
         description="Adiciona um novo serviço ao sistema.",
+        request=ServicoCriarSerializer,
         responses={
-            201: OpenApiResponse(
-                description="Serviço criado com sucesso",
-                examples=[
-                    OpenApiExample(
-                        "Sucesso",
-                        value={
-                            "id": "uuid",
-                            "nome": "Pintura",
-                            "status": True,
-                        },
-                    ),
-                ],
-            ),
+            201: ServicoCriarSerializer,
             400: OpenApiResponse(description="Dados inválidos"),
             401: OpenApiResponse(description="Credenciais inválidas"),
             500: OpenApiResponse(description="Erro no servidor"),
         },
+        examples=[
+            OpenApiExample(
+                name="Exemplo de Serviço",
+                value={"nome": "Pintar", "status": True},
+            ),
+            OpenApiExample(
+                name="Serviço criado com sucesso",
+                response_only=True,
+                status_codes=["201"],
+                value={
+                    "id": 1,
+                    "uuid": "2e7d7d7d-9b8b-4c92-9b3b-123456789abc",
+                    "nome": "Pintura",
+                    "status": True,
+                },
+            ),
+        ],
     ),
 )
