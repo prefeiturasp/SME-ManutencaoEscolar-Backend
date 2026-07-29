4e7e5f36d1c69b1ae638b096b9212fa9aeb12cd4
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-07-29 18:49:38 -0300

    feat(153163): Adiciona configurações do Celery no arquivo de settings

--- config/settings.py
+++ config/settings.py
@@ -240,3 +240,17 @@ LOGGING = {
         "level": "WARNING",
     },
 }
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "")
+
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "")
+
+CELERY_ACCEPT_CONTENT = ["json"]
+
+CELERY_TASK_SERIALIZER = "json"
+
+CELERY_RESULT_SERIALIZER = "json"
+
+CELERY_TIMEZONE = "America/Sao_Paulo"
+
+CELERY_ENABLE_UTC = False
