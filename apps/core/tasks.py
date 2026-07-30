dcaae9e4b6aef3a31b43078ad02b0a1167cfadcf
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-07-30 17:02:32 -0300

    feat(153163): Adiciona tarefa de teste do Celery e atualiza configuração do celery-beat

---
+++ apps/core/tasks.py
@@ -0,0 +1,16 @@
+"""_summary_."""
+
+from time import sleep
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+
+@shared_task
+def helchek() -> None:
+    """Task de teste do Celery."""
+    logger.info("Executando tarefa...")
+    sleep(60)
+    logger.info("Tarefa finalizada!")
