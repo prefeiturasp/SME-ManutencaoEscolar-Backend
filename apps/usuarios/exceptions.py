4c23ae18a289ad183540ef7b7c2393d47b463f73
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-08-03 17:07:43 -0300

    feat(148518): Adiciona exceção UsuarioNaoEncontradoError para tratamento de erros de usuário

---
+++ apps/usuarios/exceptions.py
@@ -0,0 +1,12 @@
+"""Erros mapedos do usuário."""
+
+
+class UsuarioNaoEncontradoError(Exception):
+    """Indica que um usuário não foi encontrado."""
+
+    def __init__(self, title: str, detail: str) -> None:
+        """Inicializa a exceção com título e descrição."""
+        self.title = title
+        self.detail = detail
+
+        super().__init__(detail)
