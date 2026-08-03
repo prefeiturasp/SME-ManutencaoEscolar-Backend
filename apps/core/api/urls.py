303ed9b32f2633d09ab866b7c02a76b2f1fbc849
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-08-03 17:27:31 -0300

    feat(148518): Adiciona rota para logout na configuração de URLs

--- apps/core/api/urls.py
+++ apps/core/api/urls.py
@@ -6,10 +6,12 @@ from apps.core.api.views import (
     AtualizarTokenView,
     HealthCheckView,
     LoginView,
+    LogoutView,
 )
 
 urlpatterns = [
     path("health/", HealthCheckView.as_view(), name="health-check"),
     path("login/", LoginView.as_view(), name="login"),
     path("refresh-token/", AtualizarTokenView.as_view(), name="refresh-token"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
