0be690cca8d83313d564fd769373ad6dd39f9ae6
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-08-07 17:37:11 -0300

    feat(148518): Atualiza testes de redefinição de senha para tratar erros de envio de e-mail e e-mail não encontrado

--- apps/core/tests/test_views.py
+++ apps/core/tests/test_views.py
@@ -407,16 +407,17 @@ def test_redefinir_senha_usuario_nao_encontrado(
     monkeypatch,
 ):
     """Deve retornar 404 quando o usuário não existir."""
+
+    def raise_envio_email_error(*args, **kwargs):
+        raise EnvioEmailError(
+            title="Erro ao enviar e-mail.",
+            detail="Parece que estamos com uma instabilidade no momento. "
+            "Tente novamnete daqui a pouco",
+        )
+
     monkeypatch.setattr(
         "apps.usuarios.api.views.UsuarioService.obter_usuario_por_rf_cpf",
-        classmethod(
-            lambda cls, _: (_ for _ in ()).throw(
-                UsuarioNaoEncontradoError(
-                    title="Usuário não encontrado.",
-                    detail="Usuário inexistente.",
-                )
-            )
-        ),
+        classmethod(raise_envio_email_error),
     )
 
     request = api_factory.post(
@@ -429,11 +430,12 @@ def test_redefinir_senha_usuario_nao_encontrado(
 
     response = RedefinirSenhaView.as_view()(request)
 
-    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
 
     assert response.data == {
-        "title": "Usuário não encontrado.",
-        "detail": "Usuário inexistente.",
+        "title": "Erro ao enviar e-mail.",
+        "detail": "Parece que estamos com uma instabilidade no momento. "
+        "Tente novamnete daqui a pouco",
     }
 
 
@@ -442,16 +444,16 @@ def test_redefinir_senha_email_nao_encontrado(
     monkeypatch,
 ):
     """Deve retornar 404 quando o usuário não possuir e-mail."""
+
+    def raise_email_usuario_error(*args, **kwargs):
+        raise EmailUsuarioNaoEncontradoError(
+            title="E-mail não encontrado.",
+            detail="Não foi encontrado e-mail para esse RF ou CPF.",
+        )
+
     monkeypatch.setattr(
         "apps.usuarios.api.views.UsuarioService.obter_usuario_por_rf_cpf",
-        classmethod(
-            lambda cls, _: (_ for _ in ()).throw(
-                EmailUsuarioNaoEncontradoError(
-                    title="E-mail não encontrado.",
-                    detail="Usuário sem e-mail.",
-                )
-            )
-        ),
+        classmethod(raise_email_usuario_error),
     )
 
     request = api_factory.post(
@@ -468,7 +470,7 @@ def test_redefinir_senha_email_nao_encontrado(
 
     assert response.data == {
         "title": "E-mail não encontrado.",
-        "detail": "Usuário sem e-mail.",
+        "detail": "Não foi encontrado e-mail para esse RF ou CPF.",
     }
 
 
