311ca02bbf081402b56cb6fc3e37f0d08e13f06c
Matheus Bonaretti <bona.matheuss2@gmail.com>
2026-09-17 16:56:52 -0300

    feat(149931): Foi realizado os ajustes para retirar o filtro do exige documento

--- apps/cargo/filters.py
+++ apps/cargo/filters.py
@@ -5,11 +5,6 @@ from django_filters import rest_framework as filters
 from apps.cargo.models import Cargo
 
 
-def _converter_exige_documento(valor: str) -> bool:
-    """Converta o exige_documento recebido como texto para booleano."""
-    return valor == "true"
-
-
 class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
     """Filtre por múltiplos valores numéricos."""
 
@@ -17,20 +12,14 @@ class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
 
 
 class CargoFilter(filters.FilterSet):
-    """Filtros disponíveis para Cargo."""
+    """Filtros disponíveis para cargos."""
 
     nome = filters.CharFilter(
         field_name="nome",
         lookup_expr="icontains",
     )
-
-    exige_documento = filters.TypedChoiceFilter(
+    exige_documento = filters.BooleanFilter(
         field_name="exige_documento",
-        choices=(
-            ("true", "Sim"),
-            ("false", "Não"),
-        ),
-        coerce=_converter_exige_documento,
     )
 
     class Meta:
