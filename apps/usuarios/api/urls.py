5b40be35bc078560f056691c384a4f7292f6c607
Priscyla C. dos Santos <santospriscyla@gmail.com>
2026-09-17 13:00:14 -0300

    feat(152372): Adiciona endpoints e filtros para cargos EOL na API

--- apps/usuarios/api/urls.py
+++ apps/usuarios/api/urls.py
@@ -2,6 +2,7 @@
 
 from rest_framework.routers import DefaultRouter
 
+from apps.usuarios.api.views.cargo_eol_views import CargoEOLViewSet
 from apps.usuarios.api.views.usuario_views import UsuarioViewSet
 
 router = DefaultRouter()
@@ -10,6 +11,11 @@ router.register(
     r"usuarios",
     UsuarioViewSet,
     basename="usuarios",
+)
+router.register(
+    r"cargos-eol",
+    CargoEOLViewSet,
+    basename="cargos-eol",
 )
 
 urlpatterns = router.urls
