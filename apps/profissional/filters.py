7a2a39a79df3c0a5ee5b80f8d9d20d7901194860
brunosp1024 <brunosalvador145@gmail.com>
2026-09-23 08:58:09 -0300

    refactor (149936): ajusta filtros para buscar profissionais por funcao

--- apps/profissional/filters.py
+++ apps/profissional/filters.py
@@ -11,10 +11,15 @@ class ProfissionalFilter(django_filters.FilterSet):
     nome = django_filters.CharFilter(lookup_expr="icontains")
     cpf = django_filters.CharFilter(lookup_expr="icontains")
     rg = django_filters.CharFilter(lookup_expr="icontains")
+    funcao = django_filters.CharFilter(
+        field_name="funcoes__cargo__nome",
+        lookup_expr="icontains",
+        distinct=True,
+    )
     status = django_filters.BooleanFilter()
 
     class Meta:
         """Configuração do filtro de profissional."""
 
         model = Profissional
-        fields = ["nome", "cpf", "rg", "status"]
+        fields = ["nome", "cpf", "rg", "funcao", "status"]
